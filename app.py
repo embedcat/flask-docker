0388baa2bbb396d8e646f26fff8d08210554cd36
kip <kip@kit-invest.ru>
2024-02-12 10:03:12 +0300

    add state, datetime

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from flask import Flask
 
 app = Flask(__name__)
@@ -11,6 +12,17 @@ def ping():
 @app.route("/api/v0/archive", methods=["POST"])
 def archive():
     return {"Result": "Ok"}
+
+
+@app.route("/api/v0/state", methods=["POST"])
+def state():
+    return {"Result": "Ok"}
+
+
+@app.route("/api/v0/datetime", methods=["GET"])
+def datetime_endpoint():
+    now = datetime.now()
+    return {"Result": "Ok", "Datetime": datetime.strftime(now, '%d.%m.%y %H:%M:%S')}
 
 
 if __name__ == "__main__":
